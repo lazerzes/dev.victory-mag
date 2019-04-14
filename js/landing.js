5d13941a060ab665990458d560d7a34f97d85803
Rei Armenia <lazerzes@users.noreply.github.com>
2019-04-14 02:32:10 -0400

    Website Update 4.14.2019 (#1)
    * seperatte js started
    
    * File Cleanup, Addition of Staff page
    
    Cleaned up JS, CSS and seperated into files based on useage.
    
    Addition of Staff.html Page, Currently a placeholder to test
    
    * Create staff.html

---
+++ js/landing.js
@@ -0,0 +1,89 @@
+$(function () {
+
+  var num_bgs_available = 8;
+  var num_bgs_displayed = 2;
+  var available_bgs = []
+  var delay_ms = 30 * 1000;
+  var new_bgs = []
+  var current_bgs = []
+
+
+  for (var i = 0; i < num_bgs_available; i++) {
+
+    s = (i + 1).toString();
+    while (s.length < 3) {
+      s = "0" + s
+    }
+
+    s = s + ".png"
+
+    available_bgs.push(s);
+
+  }
+
+  current_bgs = available_bgs.slice(0, num_bgs_displayed);
+  set_backgrounds(current_bgs);
+  setTimeout(change_backgrounds, delay_ms)
+
+  function get_new_backgrouds() {
+
+    var choices = available_bgs.slice();
+
+    //filtering out current backgrounds
+    for (var i = 0; i < current_bgs.length; i++) {
+      var x = choices.indexOf(current_bgs[i])
+      if (x !== -1) choices.splice(x, 1);
+    }
+
+    //refresh choices to default if we dot have enough. repeats possible
+    if (choices.length < num_bgs_displayed) {
+      choices = available_bgs.slice();
+    }
+
+    choices = shuffle(choices);
+    new_bgs = choices.slice(0, num_bgs_displayed)
+
+
+  }
+
+  function set_backgrounds() {
+    for (var i = 0; i < new_bgs.length; i++) {
+      var n = "bg" + i.toString();
+      var img = document.getElementById(n);
+      img.src = "img/bg/" + new_bgs[i];
+    }
+
+  }
+
+  function change_backgrounds() {
+    get_new_backgrouds();
+    $(".bg").fadeOut('fast', set_backgrounds);
+    $(".bg").fadeIn('fast');
+    current_bgs = new_bgs;
+    setTimeout(change_backgrounds, delay_ms)
+
+  }
+
+  function shuffle(array) {
+    var m = array.length,
+      t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+
+    return array;
+  }
+
+
+
+
+});
---
+++ js/paletteswap.js
@@ -0,0 +1,59 @@
+$(function () {
+
+  palette_default();
+
+});
+
+
+function next_palette() {
+
+  var palettes = [{
+    "name": "light",
+    "id": 0,
+    "background-color": "#9EE1B7",
+    "color": "#333333",
+    "banner": "0.png"
+  }, {
+    "name": "dark",
+    "id": 1,
+    "background-color": "#8F0F3F",
+    "color": "#FEFEFE",
+    "banner": "1.png"
+  }];
+
+
+  var banner = document.getElementById("banner");
+  var filename = banner.src.substring(banner.src.lastIndexOf('/') + 1);
+  var curr = parseInt(filename.slice(0, -4));
+
+  curr++;
+  curr = curr % palettes.length;
+  set_palette(banner, palettes[curr]);
+
+
+
+}
+
+function set_palette(banner, palette) {
+
+  var swapable = $('.swapable');
+  swapable.css("background-color", palette["background-color"]);
+  swapable.css("color", palette["color"]);
+
+  banner.src = "img/banner/" + palette["banner"];
+}
+
+function palette_default() {
+  p = {
+    "name": "light",
+    "id": 0,
+    "background-color": "#9EE1B7",
+    "color": "#333333",
+    "banner": "0.png"
+  }
+
+  var banner = document.getElementById("banner");
+
+  set_palette(banner, p)
+
+}
